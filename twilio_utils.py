cdcccebb08972dc42ca2d57ee2f6b52414d9a1ad
Aniket kakde <aniket.kakde@Indussoft.com>
2025-07-07 11:18:58 +0530

    deleted files

--- twilio_utils.py
+++
@@ -1,8 +0,0 @@
-import requests
-import os
-
-def download_audio_file(url):
-    auth = (os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
-    res = requests.get(url, auth=auth)
-    res.raise_for_status()
-    return res.content
